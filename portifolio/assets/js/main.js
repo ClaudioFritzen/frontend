bbb664586322c4caa145774732dabf896e8f8851
Claudio Fritzen <sdfritzen96@gmail.com>
2023-11-06 00:46:26 +0000

    feat: fizemos a função no js para filtrar e mostrar os evento de click nos projetos e skils

--- portifolio/assets/js/main.js
+++ portifolio/assets/js/main.js
@@ -1,5 +1,17 @@
 /*=============== FILTERS TABS ===============*/
+const tabs = document.querySelectorAll('[data-target]'),
+ tabsContents = document.querySelectorAll('[data-content]')
+ 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
+        tabsContents.forEach(tc =>{
+            tc.classList.remove('filters__active')
+        })
+        target.classList.add('filters__active')
+    })
+})
 
 /*=============== DARK LIGHT THEME ===============*/
 
